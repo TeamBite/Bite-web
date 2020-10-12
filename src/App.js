10c2fafbadf4e2aa5c5c7069d989a2eaf86dc449
alejo4373 <alejandrofranco@pursuit.org>
2020-10-12 01:34:39 -0400

    start using semantic ui and display app logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import 'semantic-ui-css/semantic.min.css'
+import Landing from './components/Landing';
 
 function App() {
   return (
     <div className="App">
       <h1>Bite</h1>
+      <Landing />
     </div>
   );
 }
---
+++ src/components/Landing.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container, Image } from 'semantic-ui-react'
+
+const Landing = () => {
+  return (
+    <Container>
+      <Image src="/images/bite-logo.png" size="medium" />
+
+    </Container>
+  )
+}
+
+export default Landing
