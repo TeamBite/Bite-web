0c4428f395ff4fe1470c242f3298d90e2351f5e9
alejo4373 <alejandrofranco@pursuit.org>
2020-10-12 01:40:49 -0400

    install and import react-router-dom and landing and 404 routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css'
 import Landing from './components/Landing';
+import NotFound404 from './components/NotFound404';
 
 function App() {
   return (
     <div className="App">
       <h1>Bite</h1>
-      <Landing />
+      <Switch>
+        <Route exact path="/">
+          <Landing />
+        </Route>
+        <Route>
+          <NotFound404 />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/NotFound404.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'semantic-ui-react'
+
+const NotFound404 = () => {
+  return (
+    <Container>
+      <h1>404 Not Found =(</h1>
+    </Container>
+  )
+}
+
+export default NotFound404
