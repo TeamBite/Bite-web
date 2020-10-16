fa66f1cfa672bc986ae119c59076720755f48ac0
alejo4373 <alejandrofranco@pursuit.org>
2020-10-16 02:50:33 -0400

    use NotFound404 in PatronRouter

--- src/routers/PatronRouter.jsx
+++ src/routers/PatronRouter.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import PatonLogin from '../components/PatronLogin'
 import LandingLayout from '../components/LandingLayout'
+import NotFound404 from '../components/NotFound404'
 
 const PatonRouter = () => {
   const path = '/patron'
@@ -17,6 +18,7 @@ const PatonRouter = () => {
         <Route exact path={path}>
           <PatonLogin />
         </Route>
+        <NotFound404 />
       </Switch>
     </LandingLayout >
   )
