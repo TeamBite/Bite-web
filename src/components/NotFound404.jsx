b1cc5cbe2aec1d48c7775c5ce2b168259a8d907b
alejo4373 <alejandrofranco@pursuit.org>
2020-10-16 02:45:35 -0400

    enhance NotFound404 with message emojies and link back home "/"

--- src/components/NotFound404.jsx
+++ src/components/NotFound404.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import { Container } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+import { Container, Message, Segment } from 'semantic-ui-react'
 
 const NotFound404 = () => {
   return (
     <Container>
-      <h1>404 Not Found =(</h1>
+      <Segment>
+        <Message color="pink">
+          <Message.Header> Not Found <span role="img" aria-label="warning">⚠️</span></Message.Header>
+          <p> What you were looking for was not found or is currently under construction <span role="img" aria-label="under-construction">🚧</span></p>
+        </Message>
+        <Link to="/"> ← Back Home</Link>
+      </Segment>
     </Container>
   )
 }
